8860fb896ad0819bec559c28bfff771ccd680586
Nikita Negi <nikitanegi044@gmail.com>
2024-05-16 12:37:24 +0530

    changes

--- src/components/SignCanvas.js
+++ src/components/SignCanvas.js
@@ -7,7 +7,7 @@ const SignCanvas = () => {
           <p className='description'>
             As the name suggests Signature Canvas is a signature App where you can create your digital signature and download it. You keep the background
             upload a picture to as a background. If the document that you need sign is a picture then it really makes your work easy.
-            The stack React JS,Material ui for the frontend.
+            The stack React JS and Material ui for the frontend.
             The website is deployed on firebase.
           </p>
           <a href='https://sign-canvas.web.app/' target='_blank' style={{lineHeight:1.5}}>View Site</a><br/>
